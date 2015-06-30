eb61eece30223540a18b9c73bd85a57c6382882d
oconnor-kevin <oconnor.kevin.ant@gmail.com>
2015-06-30 12:31:26 -0500

    made sure that the id's of new particles were made the same as the matter being added to

--- Matter.java
+++ Matter.java
@@ -8,13 +8,12 @@
  *
  * @author kevinoconnor
  */
-import java.util.ArrayList<E>;
-
-
+import java.util.ArrayList;
 
 public class Matter {
 // Fields
     private ArrayList<Particle> particles;
+    private int id;
     
 // Constructors
     public Matter(){
@@ -32,14 +31,21 @@ public class Matter {
     
 // Mutator Methods
     public void setParticles(ArrayList<Particle> newParticles){
+        for (int i = 0; i<newParticles.size(); i++){
+            newParticles.get(i).setID(id);
+        }
         particles = newParticles;
     }
     
     public void addParticles(ArrayList<Particle> additionalParticles){
+        for(int i = 0; i<additionalParticles.size(); i++){
+            additionalParticles.get(i).setID(id);
+        }
         particles.addAll(additionalParticles);
     }
     
     public void addParticle(Particle additionalParticle){
+        additionalParticle.setID(id);
         particles.add(additionalParticle);
     }
     
